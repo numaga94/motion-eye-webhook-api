0ffb2e06698e4d463a463d5814a9ff65850d8cf0
Shiwei Yu <shiwei1991@gmail.com>
2022-11-26 17:23:40 +0100

    1.7.0

--- main.go
+++ main.go
@@ -118,7 +118,7 @@ func main() {
 		defer res.Body.Close()
 		body, _ := io.ReadAll(res.Body)
 
-		fmt.Printf("%v 发现办公室有人或物正在移动。", strings.Replace(time.Now().Format(time.RFC3339), "T", " ", 1))
+		fmt.Printf("%v 发现办公室有人或物正在移动。\n", strings.Replace(time.Now().Format(time.RFC3339), "T", " ", 1))
 		fmt.Fprintln(w, string(body))
 	})
 
